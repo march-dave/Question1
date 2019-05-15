93475fdceb95aabf3e2fdb8e897e3c9a4df9558a
march-dave <ysangy@gmail.com>
2019-05-15 08:04:01 -0400

    code clean up

--- App.js
+++ App.js
@@ -7,34 +7,15 @@ import {
   Image,
   TouchableOpacity
 } from "react-native";
-import ActionBar from "react-native-action-bar";
 import AppDetail from "./AppDetail";
-
-import fetchCall from "./fetchCall";
-import { sortAlphabetize } from "./util";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer";
-// import { connect } from "react-redux";
-// import action from "./action";
 
 const myStore = createStore(reducer);
 
-// const mapStateToProps = state => {
-//   return {
-//     getpage: state.pages
-//   };
-// };
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     setpage: data => dispatch(action(data))
-//   };
-// };
-
 class App extends Component {
-
   render() {
     return (
       <Provider store={myStore}>
